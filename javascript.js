5d0aefed7b016eebcec155c23f53b80fac792552
Namrone <151798569+Namrone@users.noreply.github.com>
2024-04-29 21:35:14 -0700

    Created an operate function to calculate operations corretly when changing operators. Need to refine logic still

--- javascript.js
+++ javascript.js
@@ -30,7 +30,7 @@ clearBtn.addEventListener("click", function clearScreen(){
 function updateScreen(totalNum, currentNum, op){
     if(typeof currentNum == "object"){
         if(currentNum.length == 0){
-            y = 0;
+            y = "";
         }
         else
             y = parseFloat(currentNum.join(""));
@@ -45,65 +45,131 @@ function updateScreen(totalNum, currentNum, op){
 }
 
 var current = new Array();
-var operator;
+var prevOp;
 var total = 0;
 var currentInt = 0;
 
 zeroBtn.addEventListener("click", () => {
     current.push('0');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 oneBtn.addEventListener("click", () => {
     current.push('1');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 twoBtn.addEventListener("click", () => {
     current.push('2');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 threeBtn.addEventListener("click", () => {
     current.push('3');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 fourBtn.addEventListener("click", () => {
     current.push('4');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 fiveBtn.addEventListener("click", () => {
     current.push('5');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 sixBtn.addEventListener("click", () => {
     current.push('6');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 sevenBtn.addEventListener("click", () => {
     current.push('7');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 eightBtn.addEventListener("click", () => {
     current.push('8');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 nineBtn.addEventListener("click", () => {
     current.push('9');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
 decimalBtn.addEventListener("click", () => {
     current.push('.');
-    updateScreen(total, current, operator);
+    updateScreen(total, current, prevOp);
 });
 
+plusBtn.addEventListener("click", function (){operate('+');});
+subtractBtn.addEventListener("click", function (){operate('-');});
+divideBtn.addEventListener("click", function (){operate('/');});
+multiplyBtn.addEventListener("click", function () {operate('*');});
+equalBtn.addEventListener("click", function (){operate('=');});
+
+
+function operate(op){
+    if(op == '=' || prevOp == null){
+        total = parseFloat(current.join(""));
+        updateScreen("", total, op);
+        current.splice(0,current.length);
+        prevOp = op;
+    }
+    else if(prevOp == '+'){
+        if(current.length > 0){
+            currentInt = parseFloat(current.join(""));
+            total += currentInt;
+            current.splice(0,current.length);
+            prevOp = op;
+            updateScreen(total, current, prevOp);
+        }
+        else{
+            total += currentInt;
+            prevOp = op;
+            updateScreen(total, currentInt, prevOp);
+        }
+    }
+    else if(prevOp == '-'){
+        if(current.length > 0){
+            currentInt = parseFloat(current.join(""));
+            total -= currentInt;
+            current.splice(0,current.length);
+            prevOp = op;
+            updateScreen(total, current, prevOp);
+        }
+        else{
+            total -= currentInt; 
+            prevOp = op;
+            updateScreen(total, currentInt, prevOp);
+        }
+    }
+    else if(prevOp == '/'){
+        if(current.length > 0){
+            currentInt = parseFloat(current.join(""));
+            if(currentInt == 0){
+                document.querySelector(".display").innerHTML = "ERROR";
+                total = 0;
+                currentInt = 0;
+                operator = null;
+            }
+            else{
+                (operator) ?  total /= currentInt : total = currentInt;
+                operator = '/';
+                updateScreen(total, current, prevOp);
+            }
+            current.splice(0,current.length);
+        }
+        else{
+            total /= currentInt; 
+            updateScreen(total, currentInt, prevOp);
+        }
+    }
+}
+
+/*
 //Calculates addition of current numbers when + button is clicked
 plusBtn.addEventListener("click", function addition(){
     if(current.length > 0){
@@ -152,7 +218,7 @@ divideBtn.addEventListener("click", function division(){
         current.splice(0,current.length);
     }
     else{
-        total -= currentInt; 
+        total /= currentInt; 
         updateScreen(total, currentInt, operator);
     }
 });
@@ -167,7 +233,7 @@ multiplyBtn.addEventListener("click", function multiplication(){
         updateScreen(total, current, operator);
     }
     else{
-        total -= currentInt; 
+        total *= currentInt; 
         updateScreen(total, currentInt, operator);
     }
 });
@@ -176,4 +242,4 @@ multiplyBtn.addEventListener("click", function multiplication(){
 equalBtn.addEventListener("click", function equals(){
     total;
     updateScreen()
-});
+});*/
